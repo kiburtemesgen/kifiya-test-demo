7f5f528fc6f29857e6b9afb5f40ec7985269dd46
kiburtemesgen <kiburezelast@gmail.com>
2025-06-30 12:46:06 +0300

    change the app name to kifiya app

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,18 @@ import 'package:kifiya_test/core/di.dart';
 import 'package:kifiya_test/presentation/authentication/auth_bloc/auth_bloc.dart';
 import 'package:kifiya_test/presentation/authentication/login_screen.dart';
 import 'package:kifiya_test/presentation/product/blocs/favorite/favorite_bloc.dart';
-import 'package:kifiya_test/presentation/product/blocs/product/product_bloc.dart';
-import 'package:kifiya_test/presentation/product_screen.dart';
+import 'package:kifiya_test/presentation/product/blocs/product/product_bloc/product_bloc.dart';
+import 'package:kifiya_test/presentation/product/blocs/product/product_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
 
-  runApp(const MyApp());
+  runApp(const KifiyaApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class KifiyaApp extends StatelessWidget {
+  const KifiyaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
